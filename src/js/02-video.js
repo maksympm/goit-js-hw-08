f3b6edd830721ab1a00e8fc4f894ba23a2ccce08
Maksym Popov <80689833+maksympm@users.noreply.github.com>
2023-09-20 23:16:00 +0300

    2task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (time) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(time.seconds)
+    );
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
